91b8edacbc36aed39995034c7cba71f99b2f4d42
JP de la Torre <delatorre.juanpablo@gmail.com>
2022-04-15 00:22:20 -0400

    Wire add todo input box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TodosService } from './todos/services/todos.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor (
+    private todosService: TodosService,
+  ) {}
+
+  addTodo (input: HTMLInputElement) {
+    if (input.value) {
+      this.todosService.addTodo(input.value);
+      input.value = '';
+    }
+  }
+
+}
