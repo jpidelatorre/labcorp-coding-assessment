510a572b068e23fc39f19fe655972e2f89ac3083
JP de la Torre <delatorre.juanpablo@gmail.com>
2022-04-15 00:17:30 -0400

    Inlined actions to improve readability

--- src/app/todos/state/todo.actions.ts
+++ src/app/todos/state/todo.actions.ts
@@ -1,40 +1,18 @@
 import { createAction, props } from '@ngrx/store';
 import { FILTER_MODES } from '../constants/filter-modes';
 
-export const addTodo = createAction(
-  '[Todos] Add Todo',
-  props<{ text: string }>(),
-);
+export const addTodo = createAction('[Todos] Add Todo', props<{ text: string }>());
 
-export const removeTodo = createAction(
-  '[Todos] Remove Todo',
-  props<{ index: number }>(),
-);
+export const changeFilterMode = createAction('[Todos] Change Filter Mode', props<{ mode: FILTER_MODES }>());
 
-export const editTodo = createAction(
-  '[Todos] Edit Todo',
-  props<{ index: number }>(),
-);
+export const clearCompleted = createAction('[Todos] Clear Completed');
 
-export const updateTodo = createAction(
-  '[Todos] Update Todo',
-  props<{ index: number, text: string }>(),
-);
+export const editTodo = createAction('[Todos] Edit Todo', props<{ index: number }>());
 
-export const toggleCompleted = createAction(
-  '[Todos] Toggle Completed',
-  props<{ index: number }>(),
-);
+export const removeTodo = createAction('[Todos] Remove Todo', props<{ index: number }>());
 
-export const toggleAllCompleted = createAction(
-  '[Todos] Toggle All Completed',
-);
+export const toggleAllCompleted = createAction('[Todos] Toggle All Completed');
 
-export const changeFilterMode = createAction(
-  '[Todos] Change Filter Mode',
-  props<{ mode: FILTER_MODES }>(),
-);
+export const toggleCompleted = createAction('[Todos] Toggle Completed', props<{ index: number }>());
 
-export const clearCompleted = createAction(
-  '[Todos] Clear Completed',
-);
+export const updateTodo = createAction('[Todos] Update Todo', props<{ index: number, text: string }>());
